ed74a2e0f2818da1558a21b83d7f512835f34eb2
Alexandr <alysko@list.ru>
2018-01-15 00:48:21 +0300

    14-01-18

--- js/lesson1_4/js/events-dom.js
+++ js/lesson1_4/js/events-dom.js
@@ -4,11 +4,41 @@
 <input type="text"> - поле для ввода текста пользователем
 <textarea></textarea> - поле для отображения вводимого пользователем текста*/
 
-elem.onclick = function() {
-	elem.onkeydown="this.nextSibling.innerHTML = event.keyCode";
 
-};
+var text1 = document.getElementById("text");
+var text2 = document.getElementById("textarea")
 
+text1.addEventListener("keyup", copytext);
+
+function copytext() {
+	text2.setAttribute("value", text1.value); 
+} 
+
+
+
+/*(function () {
+	"use strict";
+
+	// ЗАДАЧА 1
+	// keydown, keyup, keypress
+
+	var input = document.createElement('input');
+	input.setAttribute('type', 'text');
+	input.setAttribute('value', '');
+	input.setAttribute('placeholder', 'start type something');
+
+	var textarea = document.createElement('textarea');
+
+	document.getElementById('container').appendChild(input);
+	document.getElementById('container').appendChild(textarea);
+
+	input.addEventListener('keydown', copyText);
+	input.addEventListener('keypress', copyText);
+	input.addEventListener('keyup', copyText);
+	function copyText() {
+		textarea.value = input.value;
+}
+*/
 
 /*2. Написать по одной любой функции-обработчику на следующие события:
 DOMContentLoaded
